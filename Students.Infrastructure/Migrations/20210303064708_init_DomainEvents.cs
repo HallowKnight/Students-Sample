63177a8051c8397a470f218cda7afecf19dde274
Amirmahdi Ghodsi <ghodsi.amirmahdi@yahoo.com>
2021-03-03 03:33:14 -0800

    Updated SignalR Methods

---
+++ Students.Infrastructure/Migrations/20210303064708_init_DomainEvents.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Students.Infrastructure.Migrations
+{
+    public partial class init_DomainEvents : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DomainEvent",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DomainEvent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DomainEvent_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DomainEvent_UserId",
+                table: "DomainEvent",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DomainEvent");
+        }
+    }
+}
