69f78ff94724b68f3e2599647a8663176c56b8b9
Amirmahdi Ghodsi <ghodsi.amirmahdi@yahoo.com>
2021-03-05 23:44:55 -0800

    Updated

---
+++ Students.API/Hubs/UsersListHub.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
+using Students.Application.Users.Queries.GetAllUsers;
+
+namespace Students.Presentation.Hubs
+{
+    public class UsersListHub : Hub
+    {
+
+        private readonly IMediator _mediator;
+
+        public UsersListHub(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        
+        public async Task UsersListChanged()
+        {
+            var usersList =await _mediator.Send(new GetAllUsersQuery());
+            await Clients.All.SendAsync("GetNewUsersList",usersList);
+        }
+
+        
+    }
+}
